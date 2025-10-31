d59f666ea3f71cbabc2321d097ebb8a974d37a3f
Daniel Neamu <danielneamu@gmail.com>
2025-10-31 08:42:23 +0200

    Fix formatting and reorder script includes in agents.php

--- public/agents.php
+++ public/agents.php
@@ -18,7 +18,7 @@ requireLogin();
     <link href="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.3.4/b-3.2.5/b-colvis-3.2.5/b-html5-3.2.5/b-print-3.2.5/cr-2.1.2/cc-1.1.1/date-1.6.1/fc-5.0.5/fh-4.0.4/r-3.0.7/sc-2.4.3/sb-1.8.4/sp-2.3.5/sl-3.1.3/sr-1.4.3/datatables.min.css" rel="stylesheet">
     <link href="assets/css/agents.css" rel="stylesheet" />
     <!-- Your toaslt CSS -->
-    <link href="assets/css/toast.css" rel="stylesheet">  
+    <link href="assets/css/toast.css" rel="stylesheet">
 </head>
 
 <body>
@@ -81,18 +81,18 @@ requireLogin();
         </div>
     </div>
 
-    
-        <?php
-        require_once '../includes/footer.php';
-        ?>
 
-        <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-        <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.3.4/b-3.2.5/b-colvis-3.2.5/b-html5-3.2.5/b-print-3.2.5/cr-2.1.2/cc-1.1.1/date-1.6.1/fc-5.0.5/fh-4.0.4/r-3.0.7/sc-2.4.3/sb-1.8.4/sp-2.3.5/sl-3.1.3/sr-1.4.3/datatables.min.js"></script>
-        <script src="assets/js/agents-datatable.js"></script>
-        <script src="assets/js/agents-actions.js"></script>
+    <?php
+    require_once '../includes/footer.php';
+    ?>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.3.4/b-3.2.5/b-colvis-3.2.5/b-html5-3.2.5/b-print-3.2.5/cr-2.1.2/cc-1.1.1/date-1.6.1/fc-5.0.5/fh-4.0.4/r-3.0.7/sc-2.4.3/sb-1.8.4/sp-2.3.5/sl-3.1.3/sr-1.4.3/datatables.min.js"></script>
+    <script src="assets/js/agents-datatable.js"></script>
+    <script src="assets/js/agents-actions.js"></script>
 
 
 </body>
